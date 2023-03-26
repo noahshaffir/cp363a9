daddda11fdc093cac0e848bfff28ed9d9bb14c51
noahshaffir <noahshaffir@gmail.com>
2023-03-26 16:00:24 -0400

    done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 function myfunc(){
-    alert("Developed by Ali Akbar, Noah Shaffir, Brandon Tran, and Andy Vuong, four 2nd-year Honours Computer Science students at Wilfrid Laurier University.")
+    alert("Developed by Ali Akbar, Noah Shaffir, Brandon Tran, and Andy Vuong, four 2nd-year Honours Computer Science students at Wilfrid Laurier University.");
+    //alert("Your system has been compromised. Please send 5.00 BTC to the following address: 003482828xx573yy433");
 }
